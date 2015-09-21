9aabca0bd42334888cfb0bb1e6cc131fc5ea3b29
FuGangqiang <fu_gangqiang@163.com>
2015-09-21 22:38:41 +0800

    fix #7: README.md => README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 
